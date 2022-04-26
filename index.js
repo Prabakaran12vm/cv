9851088eb76ae4d521b6940f5944511b91415940
Prabakaran12vm <104427209+Prabakaran12vm@users.noreply.github.com>
2022-04-26 15:15:14 +0530

    Add initial CV website files

---
+++ index.js
@@ -0,0 +1,78 @@
+/*var myStr = 'Pbk is mas'; 
+var charLength = myStr.length;
+var lastChar = myStr[myStr.length-2];
+console.log(myStr);
+console.log(lastChar); */
+//array
+/*var myArray = ["name", 23, "age "];
+console.log(myArray); */
+//nested array
+//push function
+/*myArray.push(["happy", "man"]);
+console.log(myArray); */
+//pop Function
+//myArray.pop();
+//shift Function
+//myArray.shift();
+//unShift Function
+/*var myArray = [20,30,40];
+myArray.unshift("10");
+console.log(myArray); 
+name = "karan";
+function myName()
+{
+ var name = "Praba";
+console.log(name);
+}
+myName();   
+//ex
+function timesFive(num)
+{
+    return (num + 5)/ 3;
+}
+total = timesFive();
+console.log(timesFive(9));
+//queue
+function myArray(arr,item)
+{
+  arr.push(item);
+     return arr.shift();
+}
+var test = [1,2,3,4,5];
+console.log("Before:" + JSON.stringify(test));
+console.log(myArray(test,6));
+console.log("After:" + JSON.stringify(test)); */
+//boolean true or false
+/*function test(equals){
+    if(equals == 12){
+    return "1";
+}
+return "no it is equal";}
+
+console.log(test("12"));*/
+function myObj (obj) {
+
+return obj.alpha;
+
+
+
+}
+    /*myObj ({
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+      "echo": "Easy",
+  "foxtrot": "Frank"}, "echo"); */
+ var obj = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+      "echo": "Easy",
+  "foxtrot": "Frank"}
+  myObj();
+
+  var testObj = {"name": "Pbk", "age": 20, "status": "single"}
+
+  
---
+++ index1.js
@@ -0,0 +1,23 @@
+
+let person = {
+     name:'praba',
+age:24
+};
+/*//Dot notation
+person.name='karan';
+//bracket notation
+let select='name';
+//person[select]='pbk';
+console.log(person[select]);
+*/
+//array
+let color = ['red', 'yellow'];
+console.log(color);
+//function
+/*function greet(name, lastname){
+     console.log('Hello ' + name +lastname);
+}
+greet('pbk ', 'legacy');
+greet('praba ', 'karan');*/
+myName = 'karan';
+console.log(myName);
